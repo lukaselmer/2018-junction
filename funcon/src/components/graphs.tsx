544f4df08d4277f19a5643a82960947ed7e6c0bd
Lukas Elmer <lukas.elmer@gmail.com>
2018-11-25 00:07:30 +0200

    improve graphs

--- funcon/src/components/graphs.tsx
+++ funcon/src/components/graphs.tsx
@@ -1,54 +1,89 @@
 import React from 'react';
 import { Bar, Pie } from 'react-chartjs-2';
 
-import { start } from '../chart';
 import { speakerStatistics } from '../text-analysis';
 
 export function Graphs() {
-  start(speakerStatistics[1].rudeWords, speakerStatistics[0].lowConfidenceWords);
-
   return (
     <div>
-      <h1 className='title'>Your Conversation Feedback</h1>
-      {drawSpeakerStatistics()}
-      {drawSwearWords()}
+      <div style={{ maxWidth: '900px', textAlign: 'center', marginLeft: 'auto', marginRight: 'auto' }}>
+        {drawSpeakerStatistics()}
+      </div>
+      <div>
+        <h3>Rudeness</h3>
+        {speakerStatistics.map(stats => (
+          <div
+            key={stats.speaker.name}
+            style={{ width: `${99.99 / speakerStatistics.length}%`, float: 'left' }}
+          >
+            {drawWordStatistics(`Conversation Rudeness of ${stats.speaker.name}`, stats.rudeWords)}
+          </div>
+        ))}
+      </div>
+      <div>
+        <h3>Conversation Confidence</h3>
+        {speakerStatistics.map(stats => (
+          <div
+            key={stats.speaker.name}
+            style={{ width: `${99.99 / speakerStatistics.length}%`, float: 'left' }}
+          >
+            {drawWordStatistics(
+              `Conversation Confidence of ${stats.speaker.name}`,
+              stats.lowConfidenceWords
+            )}
+          </div>
+        ))}
+      </div>
+      <div>
+        <h3>Parasite Word Usage</h3>
+        {speakerStatistics.map(stats => (
+          <div
+            key={stats.speaker.name}
+            style={{ width: `${99.99 / speakerStatistics.length}%`, float: 'left' }}
+          >
+            {drawWordStatistics(`Parasite Words of ${stats.speaker.name}`, stats.parasiteWords)}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 function drawSpeakerStatistics() {
   return (
-    <Pie
-      data={{
-        datasets: [
-          {
-            data: speakerStatistics.map(({ percentage }) => percentage),
-            backgroundColor: [chartColors.yellow, chartColors.green],
-            label: 'Speaker Percentage'
-          }
-        ],
-        labels: speakerStatistics.map(({ speaker: { name } }) => name)
-      }}
-      options={{
-        responsive: true
-      }}
-    />
+    <>
+      <h2 className='feedback-title'>Your Conversation Feedback</h2>
+      <h3 className='feedback-title'>Participation</h3>
+      <Pie
+        data={{
+          datasets: [
+            {
+              data: speakerStatistics.map(({ percentage }) => percentage),
+              backgroundColor: [chartColors.yellow, chartColors.green],
+              label: 'Speaker Percentage'
+            }
+          ],
+          labels: speakerStatistics.map(({ speaker: { name } }) => name)
+        }}
+        options={{
+          responsive: true
+        }}
+      />
+    </>
   );
 }
 
-function drawSwearWords() {
-  const swearWords = speakerStatistics[1].rudeWords;
-
+function drawWordStatistics(title: string, words: Map<string, number>) {
   return (
     <Bar
       data={{
-        labels: [...swearWords.keys()],
+        labels: [...words.keys()],
         datasets: [
           {
-            backgroundColor: countBackgroundColors(swearWords),
+            backgroundColor: countBackgroundColors(words),
             borderColor: chartColors.green,
             borderWidth: 1,
-            data: Array.from(swearWords.values()),
+            data: Array.from(words.values()),
             label: 'Frequency'
           }
         ]
@@ -66,7 +101,7 @@ function drawSwearWords() {
         responsive: true,
         title: {
           display: true,
-          text: 'Conversation Rudeness'
+          text: title
         }
       }}
     />
