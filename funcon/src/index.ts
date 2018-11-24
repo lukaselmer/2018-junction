9c5dfc74cbaa1d5223438feb7decc94c73a762ec
Katarina Sheremet <kate.romanowskaya@gmail.com>
2018-11-24 20:16:36 +0200

    Display confidence data in chart

--- funcon/src/index.ts
+++ funcon/src/index.ts
@@ -1,12 +1,19 @@
 import { start } from './chart';
 import {
+  firstSpeakerLowConfidenceWords,
   firstSpeakerSwearWords,
   percentageFirstSpeaker,
   percentageSecondSpeaker,
+  secondSpeakerLowConfidenceWords,
   secondSpeakerSwearWords
 } from './text-analysis';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('start stuff...');
-  start(percentageFirstSpeaker, percentageSecondSpeaker, secondSpeakerSwearWords);
+  start(
+    percentageFirstSpeaker,
+    percentageSecondSpeaker,
+    secondSpeakerSwearWords,
+    firstSpeakerLowConfidenceWords
+  );
 });
