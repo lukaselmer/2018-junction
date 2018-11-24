d93cbc283c6ebfdf3a5d82a75b36a74c9abf94dd
Katarina Sheremet <kate.romanowskaya@gmail.com>
2018-11-24 20:06:15 +0200

    Display rude words data in chart

--- funcon/src/index.ts
+++ funcon/src/index.ts
@@ -1,7 +1,12 @@
 import { start } from './chart';
-import { percentageFirstSpeaker, percentageSecondSpeaker } from './text-analysis';
+import {
+  firstSpeakerSwearWords,
+  percentageFirstSpeaker,
+  percentageSecondSpeaker,
+  secondSpeakerSwearWords
+} from './text-analysis';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('start stuff...');
-  start(percentageFirstSpeaker, percentageSecondSpeaker);
+  start(percentageFirstSpeaker, percentageSecondSpeaker, secondSpeakerSwearWords);
 });
