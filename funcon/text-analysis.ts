e370b1c54d7e4f448a06f4ae75eeddf01737c004
Marion Schleifer <marion.schleifer@gmail.com>
2018-11-24 06:53:33 +0200

    add text analysis

--- funcon/text-analysis.ts
+++ funcon/text-analysis.ts
@@ -1,3 +1,37 @@
 import { text } from "./conversation";
-console.log("Hello xx");
-console.log(text);
+
+const firstSpeakerSentences: string[] = [];
+const secondSpeakerSentences: string[] = [];
+
+text.forEach((t, i) =>
+  (i % 2 == 0 ? firstSpeakerSentences : secondSpeakerSentences).push(t)
+);
+
+const wordCountFirstSpeaker = countWordsOfSpeaker(firstSpeakerSentences);
+const wordCountSecondSpeaker = countWordsOfSpeaker(secondSpeakerSentences);
+
+const totalNumberOfWords = wordCountFirstSpeaker + wordCountSecondSpeaker;
+
+const percentageFirstSpeaker = calculatePercentage(
+  wordCountFirstSpeaker,
+  totalNumberOfWords
+);
+const percentageSecondSpeaker = calculatePercentage(
+  wordCountSecondSpeaker,
+  totalNumberOfWords
+);
+
+function countWordsOfSpeaker(sentences: string[]) {
+  return sentences.reduce((sum, sentence) => sum + wordCount(sentence), 0);
+}
+
+function wordCount(str: string) {
+  return str.split(" ").length;
+}
+
+function calculatePercentage(
+  wordCount: number,
+  totalNumberOfWords: number
+): number {
+  return Math.round((wordCount / totalNumberOfWords) * 100);
+}
