2c5ca8343f92600817341ade2a03008ae7c8d513
Lukas Elmer <lukas.elmer@gmail.com>
2018-11-25 07:14:26 +0200

    improve typings

--- funcon/src/index.tsx
+++ funcon/src/index.tsx
@@ -6,10 +6,7 @@ const root = document.getElementById('root');
 if (!root) throw new Error('Root not found');
 ReactDOM.render(<App />, root);
 
-// @ts-ignore
 if (module.hot) {
-  // @ts-ignore
   module.hot.accept();
-  // @ts-ignore
   module.hot.dispose(() => ReactDOM.unmountComponentAtNode(root));
 }
