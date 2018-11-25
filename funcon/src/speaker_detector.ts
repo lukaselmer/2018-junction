74a6bd98c736bdda562efc60e931c5d06039e5ad
Artem Sheremet <dot.doom@gmail.com>
2018-11-25 03:16:43 +0100

    Split speakerFromFFT into a separate method

--- funcon/src/speaker_detector.ts
+++ funcon/src/speaker_detector.ts
@@ -57,12 +57,14 @@ export class SpeakerDetector {
         e.inputBuffer.sampleRate
       );
 
-      const speakers = new Set<number>();
-
-      const analysisSpeaker = this.debug_drawCharts(
+      const analysisSpeaker = this.speakerFromFFT(humanSpectrum);
+      this.debug_drawCharts(
         channelData,
-        humanSpectrum.map(v => v * (humanSpectrum.length >> 4))
+        humanSpectrum.map(v => v * (humanSpectrum.length >> 4)),
+        analysisSpeaker
       );
+
+      const speakers = new Set<number>();
       if (analysisSpeaker >= 0) {
         speakers.add(analysisSpeaker);
       }
@@ -76,10 +78,33 @@ export class SpeakerDetector {
 
   private mediaStream: MediaStream | undefined;
   private mediaStreamProcessor: ScriptProcessorNode | undefined;
+
   private imprintIndex = 0;
   private imprintDirection = 1;
+  private imprintPixel: ImageData | undefined;
 
-  private debug_drawCharts(pcmData: Float32Array, fftData: number[]) {
+  private speakerFromFFT(fftData: number[]) {
+    const fffft = fourierTransform(
+      fftData
+        .slice(0, Math.pow(2, Math.floor(Math.log2(fftData.length))))
+        .map(v => v * (fftData.length >> 4))
+    ) as number[];
+
+    const sorted4ft = fffft.map((v, index) => [v, index]).sort((v1, v2) => v2[0] - v1[0]);
+    let speaker = -1;
+    for (let i = 0; i < sorted4ft.length / 8; ++i) {
+      if (sorted4ft[i][0] > 0.04) {
+        speaker = 0;
+        if (sorted4ft[i][1] > sorted4ft.length / 4) {
+          speaker = 1;
+          break;
+        }
+      }
+    }
+    return speaker;
+  }
+
+  private debug_drawCharts(pcmData: Float32Array, fftData: number[], speaker: number) {
     const getCanvasAndContext = (name: string, shouldClear: boolean) => {
       const canvas = document.getElementById(name) as HTMLCanvasElement | null;
       if (!canvas) {
@@ -104,7 +129,7 @@ export class SpeakerDetector {
     };
 
     const setPixel = (canvas: CanvasRenderingContext2D, x, y, intencity: number) => {
-      const pixel = canvas.createImageData(1, 1);
+      const pixel = (this.imprintPixel = this.imprintPixel || canvas.createImageData(1, 1));
       const pixelData = pixel.data;
 
       let totalIntencity = (intencity + 0.5) * 255 * 3;
@@ -138,25 +163,6 @@ export class SpeakerDetector {
     const fftBarWidth = fft.el.width / fftData.length,
       fftTickStep = Math.floor(50 / fftBarWidth);
 
-    const fffft = fourierTransform(
-      fftData.slice(0, Math.pow(2, Math.floor(Math.log2(fftData.length))))
-    ) as number[];
-    const sorted4ft = fffft.map((v, index) => [v, index]).sort((v1, v2) => v2[0] - v1[0]);
-    let speaker = -1;
-    for (let i = 0; i < sorted4ft.length / 8; ++i) {
-      if (sorted4ft[i][0] > 0.04) {
-        speaker = 0;
-        if (sorted4ft[i][1] > sorted4ft.length / 4) {
-          speaker = 1;
-          break;
-        }
-      }
-    }
-
-    while (fftData.length > fffft.length) {
-      fffft.push(0);
-    }
-
     fft.ctx.beginPath();
     fftData.forEach((value, index) => {
       const y = fft.el.height * (1 - value);
@@ -165,20 +171,22 @@ export class SpeakerDetector {
         fft.ctx.strokeText(index.toString(), fftBarWidth * (index + 0.5), 10);
       }
       fft.ctx.lineTo(fftBarWidth * (index + 1), y);
-      // setPixel(imprint.ctx, this.imprintIndex, index, fffft[index] * 4);
       setPixel(imprint.ctx, this.imprintIndex, index, fftData[index] * 4);
     });
     fft.ctx.stroke();
 
     imprint.ctx.lineWidth = 2;
     imprint.ctx.beginPath();
-    imprint.ctx.moveTo(this.imprintIndex + this.imprintDirection * 3, 0);
-    imprint.ctx.lineTo(this.imprintIndex + this.imprintDirection * 3, imprint.el.height);
+    imprint.ctx.moveTo(this.imprintIndex + this.imprintDirection * (imprint.ctx.lineWidth + 1), 0);
+    imprint.ctx.lineTo(
+      this.imprintIndex + this.imprintDirection * (imprint.ctx.lineWidth + 1),
+      imprint.el.height
+    );
     imprint.ctx.stroke();
 
-    setPixel(imprint.ctx, this.imprintIndex, 0, (speaker + 1) / 4);
-
-    return speaker;
+    for (let i = imprint.el.height * 0.95; i < imprint.el.height; ++i) {
+      setPixel(imprint.ctx, this.imprintIndex, i, (speaker + 1) / 4);
+    }
   }
 
   stop() {
